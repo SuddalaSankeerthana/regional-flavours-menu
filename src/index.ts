ac0add2dfa2df3fe08945324a6222e7c89a0873e
Sankeerthana <sankeerthanasuddala@Sankeerthanas-MacBook-Pro.local>
2023-11-10 16:36:54 +0530

    Added a get method for the API

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-const express = require("express");
+import express from "express";
 import getMenuData from "./data/menu";
+
+const PORT = 3000;
+
 const app = express();
-const port = 3000;
 let menu = getMenuData();
+
 app.use(express.json());
-module.exports = app;
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
 });
 
 app.get("/menu", (request: Request, response: any) => {
-  console.log(menu);
   response.status(200).send(menu);
 });
+
 export default app;
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,7 +2,7 @@ import kill from "kill-port";
 import request from "supertest";
 import app from "../index";
 const res = request(app);
-describe("Server get method testing", () => {
+describe("Server get and post methods testing", () => {
   it("should return data", async () => {
     const status = (await res.get("/menu")).statusCode;
     expect(status).toBe(200);
